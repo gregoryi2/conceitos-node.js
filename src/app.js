e4158a10388e826adce037c062d41df57dc7a22d
gregoryi2 <gregory.pereira2@gmail.com>
2020-08-19 17:20:19 -0300

    Projects CRUD, Likes

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const { uuid } = require("uuidv4");
 
 // const { v4: uuid } = require('uuid');
 
@@ -11,23 +12,74 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const {title, url, techs} = request.body;
+
+  const repository = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes: 0,
+  }
+
+  repositories.push(repository);
+
+  return response.json(repository);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+ const {title, url, techs} = request.body;
+ const {id} = request.params;
+
+ const repository = repositories.find(repository => repository.id === id);
+
+ if(!repository){
+   response.status(400).json({error: 'Could not find respository.'});
+ }
+
+ const newRepository = {
+     id,
+     title,
+     url,
+    techs,
+    likes: repository.likes,
+ }
+
+  repositories.push(newRepository)
+
+  return response.json(newRepository);
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const {id} = request.params;
+
+  const repository = repositories.find(repository => repository.id === id);
+
+  if(!repository){
+    response.status(400).json({error: 'Could not delete respository.'});
+  };
+
+  repositories.splice(repository);
+
+  return response.status(204).send();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const {id} = request.params;
+
+  const repository = repositories.find(repository => repository.id === id);
+
+  if(!repository){
+    response.status(400).json({error: 'Could not find respository.'});
+  }
+
+  repository.likes += 1;
+
+  return response.json(repository);
 });
 
 module.exports = app;
